612e23c1714d833d10cb2d2c986036eb1c7c7037
TheSintBex <jmsaez2712@ieszaidinvergeles.org>
2022-05-14 16:31:00 +0200

    Cambiado youtube-dl a yt-dlp

--- skills/youtube.py
+++ skills/youtube.py
@@ -1,11 +1,13 @@
 import os
 
-import youtube_dl
+# import youtube_dl
+from yt_dlp import YoutubeDL
 import re
 from urllib import parse, request
+import soundManager as sm
 
 ydl_options = {
-    'format': 'bestaudio/best',
+    'format': 'mp3/bestaudio',
     'postprocessor': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -26,6 +28,7 @@ class Skill():
 
     @staticmethod
     def init(intent):
+        os.remove("song.mp3")
         search = intent.placeHolders['song']
         query_string = parse.urlencode({'search_query': search})
         html_content = request.urlopen('https://www.youtube.com/results?' + query_string)
@@ -33,11 +36,12 @@ class Skill():
         print(search_results)
         video_url = "https://www.youtube.com/watch?v=" + search_results[0]
         print(video_url)
-        with youtube_dl.YoutubeDL(ydl_options) as ydl:
+        with YoutubeDL(ydl_options) as ydl:
             ydl.download([video_url])
         for file in os.listdir("./"):
-            if file.endswith(".mp3"):
+            if file.endswith(".webm") | file.endswith(".m4a"):
                 os.rename(file, "song.mp3")
+        sm.Mixer.playMusic("song.mp3")
 
     @staticmethod
     def skill_output():
